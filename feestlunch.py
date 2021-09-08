20a5d8f427b0c3090dc0f91310e3ac9c4b1b16d7
unknown <gorinchem1995@hotmail.com>
2021-09-08 13:45:45 +0200

    ?

---
+++ feestlunch.py
@@ -0,0 +1,10 @@
+Crossant  = 0.39
+HoeveelCrossant = 17
+Stokbrood = 2.78
+HoeveelStokbrood = 2
+Kortingsbon = 1.50
+Totaal = 12.19
+
+print('17 Crossantjes kosten:' + str(Crossant * HoeveelCrossant))
+print('2 Stokbroodjes kosten:' + str(Stokbrood * HoeveelStokbrood))
+print('Totaal Prijs met 1,50 € korting:' + str(Totaal - Kortingsbon))
---
+++ speeldag.py
@@ -0,0 +1,10 @@
+Ticket = 7.45
+Vrienden = 3
+VIP = 45
+VIPMinuut = 0.37
+Tickets3 = 22.35
+VR = 16.65
+
+print('3 tickets kosten:' + str(Ticket * Vrienden))
+print('45 minuten VIP VR kost:' + str(VIP *  VIPMinuut))
+print('Alles bijelkaar kost :' + str(Tickets3 + VR))
