a20b08e2fe408a86adf30123d9e0d3a4f9248ab5
unknown <gorinchem1995@hotmail.com>
2021-09-10 12:06:49 +0200

    netto

--- feestlunch.py
+++ feestlunch.py
@@ -4,7 +4,12 @@ Stokbrood = 2.78
 HoeveelStokbrood = 2
 Kortingsbon = 1.50
 Totaal = 12.19
+MetKorting = 10.69
+
+
 
 print('17 Crossantjes kosten:' + str(Crossant * HoeveelCrossant))
 print('2 Stokbroodjes kosten:' + str(Stokbrood * HoeveelStokbrood))
 print('Totaal Prijs met 1,50 € korting:' + str(Totaal - Kortingsbon))
+
+print ('De feestlunch kost je bij de bakker ' + str(MetKorting) + ' voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
--- speeldag.py
+++ speeldag.py
@@ -4,7 +4,10 @@ VIP = 45
 VIPMinuut = 0.37
 Tickets3 = 22.35
 VR = 16.65
+Totaal = 39.00
 
 print('3 tickets kosten:' + str(Ticket * Vrienden))
 print('45 minuten VIP VR kost:' + str(VIP *  VIPMinuut))
 print('Alles bijelkaar kost :' + str(Tickets3 + VR))
+
+print ('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar ' +str (Totaal) +('€'))
